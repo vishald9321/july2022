abfe95435ec7669df761f803531cbd6a74482885
SHEETAL <SHEETAL@SHITALPC>
2022-07-25 14:25:01 +0530

    this is second commit

--- src/Demo/sample2.java
+++ src/Demo/sample2.java
@@ -1,5 +1,12 @@
 package Demo;
 
-public class sample2 {
-
+public class sample2
+{
+      public static void main(String[] args) 
+      {
+		int p=50;
+		int q=30;
+		int sub=p-q;
+		System.out.println(sub);
+	}
 }
